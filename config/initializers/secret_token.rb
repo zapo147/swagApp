50e28162eed67625f0977df6b175641f80bc97cf
eric <eldpeppler@gmail.com>
2014-03-19 20:26:14 -0400

    intialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwagApp::Application.config.secret_key_base = 'a8ecb07969ed179a03f1c460ec42ba2d7acd1459a35f3503395e2f44a39989b9f57ddae19de6b93a7e62e8a6b58412b4589f521abef2d7aed6ebfe7ea4bbf2ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SwagApp::Application.config.session_store :cookie_store, key: '_swagApp_session'
